aed173c9eddbc837ec638f2527a901c1fca9a121
darcyTwo <3538356136@qq.com>
2022-10-15 22:20:07 +0800

    Feature:complete the add user function in the Role component

--- src/pages/Role/index.jsx
+++ src/pages/Role/index.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import {
   HomeOutlined, UserOutlined, PictureOutlined, FrownOutlined, IdcardOutlined, HddOutlined, GatewayOutlined, FormOutlined, DeleteOutlined, SettingOutlined
 } from '@ant-design/icons';
-import { Breadcrumb, Card, Table, Divider, Radio, Input, message, Switch, Tooltip, Popconfirm } from 'antd';
-import { getUserListMethod, switchUserState, deleteCurrentUser } from '../../api';
+import { Breadcrumb, Card, Table, Divider, Radio, Input, message, Switch, Tooltip, Popconfirm, Modal, Form, Select } from 'antd';
+import { getUserListMethod, switchUserState, deleteCurrentUser, addUser } from '../../api';
 import { formateDate } from '../../utils/formateDate';
 import './index.css'
 import BreadcrumbItem from 'antd/lib/breadcrumb/BreadcrumbItem';
+import Draggable from 'react-draggable';
 const { Search } = Input;
+const { Option } = Select;
 //角色路由
 const TitleIcon = [
   {
@@ -53,13 +55,21 @@ const TitleIcon = [
     path: '/charts'
   }
 ]
-
-
 const rowSelection = {
   onChange: (selectedRowKeys, selectedRows) => {
     console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
   },
 };
+//验证手机号的规则
+const MobileRule = (rule, value, callback) => {
+  console.log(callback, 'callback');
+  const regMobile = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
+  if (regMobile.test(value)) {
+    return callback();
+  }
+  callback(new Error('Please input the correct mobile number!'));
+}
+//验证规则
 
 class Role extends Component {
   constructor(props) {
@@ -83,6 +93,23 @@ class Role extends Component {
         pageSizeOptions: [2, 3, 4, 5],
       },
       loading: false,
+      //弹窗的属性
+      open: false,//模态弹窗的状态
+      disabled: false,
+      //弹窗的位置
+      bounds: {
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0
+      },
+      //添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      }
     }
   }
 
@@ -139,10 +166,6 @@ class Role extends Component {
       message.success('更新用户状态信息成功!');
     }
   }
-  //点击添加用户
-  handleCreateRole = () => {
-    console.log('添加用户');
-  }
   //确认删除
   onConfirm = async (currentUserInfo) => {
     const { data: res } = await deleteCurrentUser(currentUserInfo.id);
@@ -156,8 +179,55 @@ class Role extends Component {
   onCancel = () => {
     message.success('取消删除成功!');
   }
+  //弹窗事件
+  //点击添加用户
+  handleCreateRole = () => {
+    this.setState({
+      open: true
+    })
+  }
+  handleOk = async () => {
+    const { data: res } = await addUser(this.formRef.current.getFieldsValue());
+    if (res.meta.status !== 201) {
+      message.error('添加用户失败!');
+    }
+    message.success('添加用户成功!');
+    this.getUserList();
+    this.setState({
+      open: false
+    })
+
+  }
+  handleCancel = () => {
+    message.success('取消添加用户!')
+    this.setState({
+      open: false
+    })
+  }
+  onFinish = (value) => {
+    console.log('Finish', value);
+  }
+  //弹窗ref
+  draggleRef = React.createRef();
+  //表单ref
+  formRef = React.createRef();
+  onStart = (_event, uiData) => {
+    const { clientWidth, clientHeight } = window.document.documentElement;
+    const targetRect = this.draggleRef.current?.getBoundingClientRect();
+    if (!targetRect) {
+      return;
+    }
+    this.setState({
+      bounds: {
+        left: -targetRect.left + uiData.x,
+        right: clientWidth - (targetRect.right - uiData.x),
+        top: -targetRect.top + uiData.y,
+        bottom: clientHeight - (targetRect.bottom - uiData.y),
+      }
+    })
+  };
   render() {
-    const { selectionType, usersList, pagination, loading } = this.state;
+    const { selectionType, usersList, pagination, loading, open, disabled, bounds } = this.state;
     const columns = [
       {
         title: 'ID',
@@ -237,8 +307,14 @@ class Role extends Component {
         <Search placeholder="input search text" enterButton size='small' style={{ width: '600px' }} />
         <button className='create_role' onClick={this.handleCreateRole}>创建角色</button>
       </div>
-
     )
+    const prefixSelector = (
+      <Form.Item name="prefix" noStyle initialValue="+86">
+        <Select style={{ width: 70 }}>
+          <Option value="87">+87</Option>
+        </Select>
+      </Form.Item>
+    );
     return (
       <div className='RoleContent' >
         <div className='RoleTop'>
@@ -284,6 +360,131 @@ class Role extends Component {
             />
           </Card>
         </section>
+        <Modal
+          title={
+            <div
+              style={{
+                width: '100%',
+                cursor: 'move',
+              }}
+              onMouseOver={() => {
+                if (disabled) {
+                  this.setState({
+                    disabled: false
+                  })
+                }
+              }}
+              onMouseOut={() => {
+                this.setState({
+                  disabled: true
+                })
+              }}
+              onFocus={() => { }}
+              onBlur={() => { }}
+            >
+              添加用户
+            </div>
+          }
+          open={open}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          modalRender={(modal) => (
+            <Draggable
+              disabled={disabled}
+              bounds={bounds}
+              onStart={(event, uiData) => this.onStart(event, uiData)}
+            >
+              <div ref={this.draggleRef}>{modal}</div>
+            </Draggable>
+          )}
+        >
+          <Form ref={this.formRef} onFinish={this.onFinish}>
+            <Form.Item
+              name="username"
+              label="Name"
+              rules={[
+                {
+                  required: true, message: 'The username lenght at least four characters!'
+                }, {
+                  min: 4, message: 'The username lenght at least four characters!'
+                }, {
+                  max: 12, message: 'The username length at most twelve characters!'
+                }, {
+                  pattern: /^[a-zA-Z0-9_]+$/, message: 'The username is made of English,digitals,and underscroes'
+                }
+              ]}
+            >
+              <Input type='text' maxLength={17} />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true, message: 'Please input your password!',
+                }, {
+                  min: 4, message: 'The username lenght at least four characters!'
+                }, {
+                  max: 12, message: 'The username length at most twelve characters!'
+                }, {
+                  pattern: /^[a-zA-Z0-9_]+$/, message: 'The username is made of English,digitals,and underscroes'
+                }
+              ]}
+              hasFeedback
+            >
+              <Input.Password maxLength={17} />
+            </Form.Item>
+            <Form.Item
+              name="confirm"
+              label="Confirm Password"
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="phone"
+              label="Phone Number"
+              rules={[
+                { required: true, message: 'Please input your phone number!' },
+                { validator: MobileRule, validateTrigger: 'blur' }
+              ]}
+            >
+              <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+            </Form.Item>
+
+          </Form>
+        </Modal>
       </div >
     )
   }
